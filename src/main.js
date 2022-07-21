519198405c89e443d6febcf15f7651bcc2456604
marumuji <marumuji@gmail.com>
2022-07-21 23:08:01 +0900

    Add Apollo client

--- src/main.js
+++ src/main.js
@@ -1,4 +1,29 @@
-import { createApp } from 'vue'
+import { createApp, provide, h } from 'vue'
+import { DefaultApolloClient } from '@vue/apollo-composable'
 import App from './App.vue'
+import {
+  AppoloClient,
+  createHttpLink,
+  InMemoryCache
+} from '@apollo/client/core'
 
-createApp(App).mount('#app')
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000/',
+})
+
+const cache = new InMemoryCache()
+
+const apolloClient = new AppoloClient({
+  link: httpLink,
+  cache,
+})
+
+const app = createApp({
+  setup() {
+    provide(DefaultApolloClient, apolloClient)
+  },
+  render: () => h(App),
+})
+
+app.mount('#app')
+
